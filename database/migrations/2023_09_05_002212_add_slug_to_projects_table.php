46c5c69c4204fabfa5ac8e9090328bc799e9f303
SteCard <stefanocardillo14@gmail.com>
2023-09-06 18:05:47 +0200

    Slug + api fix

--- database/migrations/2023_09_05_002212_add_slug_to_projects_table.php
+++ database/migrations/2023_09_05_002212_add_slug_to_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->string('slug', 50)->nullable()->after('title');
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('projects', function (Blueprint $table) {
-            //
+            $table->dropColumn('slug');
         });
     }
 };
